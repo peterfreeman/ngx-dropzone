766907c13c69d6f83e40a8c652f0ddaf4c0bf2f5
Peter Freeman <peterfreeman176@gmail.com>
2019-07-16 21:59:23 +0200

    docs: Added first draft of feature requirements. feat: Added empty label component.

---
+++ projects/ngx-dropzone/src/lib/ngx-dropzone-label/ngx-dropzone-label.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-dropzone-label',
+  templateUrl: './ngx-dropzone-label.component.html',
+  styleUrls: ['./ngx-dropzone-label.component.scss']
+})
+export class NgxDropzoneLabelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- projects/ngx-dropzone/src/lib/ngx-dropzone.module.ts
+++ projects/ngx-dropzone/src/lib/ngx-dropzone.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { NgxDropzoneComponent } from './ngx-dropzone.component';
 import { CommonModule } from '@angular/common';
+import { NgxDropzonePreviewComponent } from './ngx-dropzone-preview/ngx-dropzone-preview.component';
+import { NgxDropzoneLabelComponent } from './ngx-dropzone-label/ngx-dropzone-label.component';
+import { NgxDropzoneImagePreviewComponent } from './ngx-dropzone-image-preview/ngx-dropzone-image-preview.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    NgxDropzoneComponent
+    NgxDropzoneComponent,
+    NgxDropzonePreviewComponent,
+    NgxDropzoneLabelComponent,
+    NgxDropzoneImagePreviewComponent
   ],
   exports: [
     NgxDropzoneComponent
