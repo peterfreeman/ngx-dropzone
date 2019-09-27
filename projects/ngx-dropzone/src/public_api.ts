2ffc8e01878d5e69ef9dbb2d012c332fd106f74f
Peter Freeman <peterfreeman176@gmail.com>
2019-09-27 21:33:34 +0200

    fix: Extended the public API to allow for preview component inheritance.

--- projects/ngx-dropzone/src/public_api.ts
+++ projects/ngx-dropzone/src/public_api.ts
@@ -3,3 +3,8 @@
  */
 
 export * from './lib/ngx-dropzone.module';
+export * from './lib/ngx-dropzone/ngx-dropzone.component';
+export * from './lib/ngx-dropzone-preview/ngx-dropzone-preview.component';
+export * from './lib/ngx-dropzone-preview/ngx-dropzone-image-preview/ngx-dropzone-image-preview.component';
+export * from './lib/ngx-dropzone-preview/ngx-dropzone-video-preview/ngx-dropzone-video-preview.component';
+export * from './lib/ngx-dropzone-preview/ngx-dropzone-remove-badge/ngx-dropzone-remove-badge.component';
