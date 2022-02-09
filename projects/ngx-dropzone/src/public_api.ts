b5ea2f156024b869dc10cb87e354719e3fab43f3
peterfreeman <peterfreeman176@gmail.com>
2022-02-09 17:16:12 +0100

    build: Updated dependencies and now uses Ivy.

--- projects/ngx-dropzone/src/public_api.ts
+++ projects/ngx-dropzone/src/public_api.ts
@@ -3,6 +3,7 @@
  */
 
 export * from './lib/ngx-dropzone.module';
+export * from './lib/ngx-dropzone-label.directive';
 export * from './lib/ngx-dropzone/ngx-dropzone.component';
 export * from './lib/ngx-dropzone-preview/ngx-dropzone-preview.component';
 export * from './lib/ngx-dropzone-preview/ngx-dropzone-image-preview/ngx-dropzone-image-preview.component';
